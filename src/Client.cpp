c7c35d1a206cd0a3d2869331a3888f5e347ad864
mason <mason@pisscake.com>
2014-08-30 19:23:47 -0700

    well... i got something to work

--- src/Client.cpp
+++ src/Client.cpp
@@ -17,21 +17,22 @@ void Client::start(void){
 		udp::socket s(io_service, udp::endpoint(udp::v4(), 0));
 
 		udp::resolver resolver(io_service);
-		udp::resolver::query query(udp::v4(), HolePunchHostName,
-			std::to_string(port));
+		udp::resolver::query query(udp::v4(), HolePunchHostName, "2048");
 		udp::resolver::iterator iterator = resolver.resolve(query);
 
-		using namespace std; // For strlen.
-		std::cout << "Enter message: ";
-		char request[max_length];
-		std::cin.getline(request, max_length);
-		size_t request_length = strlen(request);
+		//char request[1024];
+		std::cout << "Enter message: " << std::endl;
+		std::string request;
+		std::cin >> request;
+		std::cout << "requesting: " << request << std::endl;
+
+		size_t request_length = request.length();
 		s.send_to(boost::asio::buffer(request, request_length), *iterator);
 
-		char reply[max_length];
+		char reply[1024];
 		udp::endpoint sender_endpoint;
 		size_t reply_length = s.receive_from(
-			boost::asio::buffer(reply, max_length), sender_endpoint);
+			boost::asio::buffer(reply, 1024), sender_endpoint);
 		std::cout << "Reply is: ";
 		std::cout.write(reply, reply_length);
 		std::cout << "\n";
--- srv/src/HolePunchServer.cpp
+++ srv/src/HolePunchServer.cpp
@@ -10,30 +10,22 @@ HolePunchServer::~HolePunchServer(void){
 }
 
 void HolePunchServer::start(void){
-	try{
-		boost::asio::io_service io_service;
+	boost::asio::io_service io;
 
-		udp::socket s(io_service, udp::endpoint(udp::v4(), 0));
-
-		udp::resolver resolver(io_service);
-		udp::resolver::query query(udp::v4(), argv[1], argv[2]);
-		udp::resolver::iterator iterator = resolver.resolve(query);
-
-		using namespace std; // For strlen.
-		std::cout << "Enter message: ";
-		char request[max_length];
-		std::cin.getline(request, max_length);
-		size_t request_length = strlen(request);
-		s.send_to(boost::asio::buffer(request, request_length), *iterator);
-
-		char reply[max_length];
+	udp::socket socket(io, udp::endpoint(udp::v4(), port));
+	//int max_length = 1024;
+	while(true){
+		//std::string buffer;
 		udp::endpoint sender_endpoint;
-		size_t reply_length = s.receive_from(
-			boost::asio::buffer(reply, max_length), sender_endpoint);
-		std::cout << "Reply is: ";
-		std::cout.write(reply, reply_length);
-		std::cout << "\n";
-	}catch (std::exception& e){
-		std::cerr << "Exception: " << e.what() << "\n";
+		std::vector<char> buffer(1024);
+		socket.receive_from(boost::asio::buffer(buffer), sender_endpoint);
+
+		std::cout << "recieved: \"";
+		for(unsigned int i = 0; i < buffer.size(); i++){
+			std::cout << buffer.at(i);
+		}
+		std::cout << "\" from: " << sender_endpoint.address() << std::endl;
+
+		socket.send_to(boost::asio::buffer(buffer), sender_endpoint);
 	}
 }
